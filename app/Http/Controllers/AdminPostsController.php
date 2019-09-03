c1621c55bde24717a2aaad4f4cbb5fa6f28009de
shlok sharma <shloksharma347@gmail.com>
2019-09-03 18:34:21 +0530

    Categories Added

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Photo;
+use App\Category;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\PostsCreateRequest;
 
@@ -28,7 +29,8 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::pluck('name', 'id')->all();
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
